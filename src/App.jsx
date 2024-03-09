3e2b310404417b290087e36f0fc0768a3f19a71b
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-03-09 20:05:22 +0800

    feat: name can now be changed and rooms can be joined

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,35 @@ const socket = io.connect('http://localhost:3001')
 
 function App() {
 
+  const [name, setName] = useState('')
   const [users, setUsers] = useState([])
   const [roomToJoin, setRoomToJoin] = useState('')
 
-  let setUsername = () => {
-    socket.emit('send-nickname', 'Zakary')
-  }
-
-  socket.on('users-list', function(users) {
-    console.log(users)
+  socket.on('users', function(users) {
     setUsers(users)
   })
 
   let joinRoom = () => {
-    socket.emit('join-room', roomToJoin)
+    socket.emit('join-room', {name, roomToJoin})
   }
 
   return (
     <>
       <h1>Codenames</h1>
       <h2>Built with React, Node and Socket.IO</h2>
-      <button onClick={setUsername}>Set Nickname</button>
-      {users.map(user => (
+      <div>
+        <h3>Enter Username</h3>
+        <input value={name} onChange={(event) => setName(event.target.value)} />
+      </div>
+      <div>
+        <h3>Enter Room Code</h3>
+        <input value={roomToJoin} onChange={(event) => setRoomToJoin(event.target.value)} />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      {users.length > 0 && <h3>Joined Users:</h3>}
+      {users.length > 0 && users.map(user => (
         <h4 key={user}>{user}</h4>
       ))}
-      <input value={roomToJoin} onChange={(event) => setRoomToJoin(event.target.value)} />
-      <button onClick={joinRoom}>Join Room</button>
     </>
   )
 }
