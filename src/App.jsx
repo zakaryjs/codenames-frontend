a8fd9aee2861344574395ff952cbc67f81f3b9c3
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-03-09 19:10:13 +0800

    feat: nicknames and rooms working - hardcoded

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import io from 'socket.io-client'
+const socket = io.connect('http://localhost:3001')
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [users, setUsers] = useState([])
+  const [roomToJoin, setRoomToJoin] = useState('')
+
+  let setUsername = () => {
+    socket.emit('send-nickname', 'Zakary')
+  }
+
+  socket.on('users-list', function(users) {
+    console.log(users)
+    setUsers(users)
+  })
+
+  let joinRoom = () => {
+    socket.emit('join-room', roomToJoin)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Codenames</h1>
+      <h2>Built with React, Node and Socket.IO</h2>
+      <button onClick={setUsername}>Set Nickname</button>
+      {users.map(user => (
+        <h4 key={user}>{user}</h4>
+      ))}
+      <input value={roomToJoin} onChange={(event) => setRoomToJoin(event.target.value)} />
+      <button onClick={joinRoom}>Join Room</button>
     </>
   )
 }
